c9a1cf1cde3fd978dceb865112344fc40f27f8ec
Thiago Perrotta <tbperrotta@gmail.com>
2021-12-09 23:49:22 -0500

    add day 5 part 2: python

--- day5/main.py
+++ day5/main.py
@@ -1,31 +1,65 @@
 #!/usr/bin/env python3
 from collections import defaultdict
+from itertools import count, islice
 import sys
 
 with open(sys.argv[1]) as input:
     lines = input.read().splitlines()
 
-def fill(floor, first, second):
-    if first[0] == second[0]:
-        # print('vertical line:', f'first, second = {first}, {second}')
+def direction(a, b):
+    """
+    3, 5 => +1
+    5, 3 => -1
+    5, 5 => 0
+    """
+    return 1 if b > a else -1 if a > b else 0
 
-        for j in range(*[el + 1 if i == 1 else el for i, el in enumerate(sorted([first[1], second[1]]))]):
-            # print(f'fill: <{first[0]}, {j}>')
-            floor[(first[0], j)] += 1
-    elif first[1] == second[1]:
-        # print('horizontal line:', f'first, second = {first}, {second}')
-        for i in range(*[el + 1 if i == 1 else el for i, el in enumerate(sorted([first[0], second[0]]))]):
-            # print(f'fill: <{i}, {first[1]}>')
-            floor[(i, first[1])] += 1
+def fill_part1(floor, first, second):
+    [x1, y1] = first
+    [x2, y2] = second
 
-floor = defaultdict(int)
+    # vertical line
+    if x1 == x2:
+        for y in list(range(*sorted([y1, y2]))) + [max(y1, y2)]:
+            floor[(x1, y)] += 1
 
-for line in lines:
-    first, second = [pair.split(',') for pair in line.split(' -> ')]
+    # # horizontal line
+    elif y1 == y2:
+        for x in list(range(*sorted([x1, x2]))) + [max(x1, x2)]:
+            floor[(x, y1)] += 1
 
-    first = list(map(int, first))
-    second = list(map(int, second))
+def fill_part2(floor, first, second):
+    [x1, y1] = first
+    [x2, y2] = second
 
-    fill(floor, first, second)
+    for x, y in islice(zip(count(start = x1, step = direction(x1, x2)), count(start = y1, step = direction(y1, y2))), max(abs(x2 - x1), abs(y2 - y1)) + 1):
+        floor[(x, y)] += 1
 
-print(sum(el >= 2 for el in list(floor.values())))
+def part1():
+    floor = defaultdict(int)
+
+    for line in lines:
+        first, second = [pair.split(',') for pair in line.split(' -> ')]
+
+        first = list(map(int, first))
+        second = list(map(int, second))
+
+        fill_part1(floor, first, second)
+
+    print(sum(el >= 2 for el in floor.values()))
+
+def part2():
+    floor = defaultdict(int)
+
+    for line in lines:
+        first, second = [pair.split(',') for pair in line.split(' -> ')]
+
+        first = list(map(int, first))
+        second = list(map(int, second))
+
+        fill_part2(floor, first, second)
+
+    print(sum(el >= 2 for el in floor.values()))
+
+part1()
+part2()

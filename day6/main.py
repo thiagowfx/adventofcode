70759db96f7b1f5e022d0713b217b62a32816826
Thiago Perrotta <tbperrotta@gmail.com>
2021-12-10 20:04:38 -0500

    add day 6 part 2: python

--- day6/main.py
+++ day6/main.py
@@ -5,16 +5,23 @@ import sys
 with open(sys.argv[1]) as input:
     numbers = list(map(int, input.read().split(',')))
 
-fish = Counter(numbers)
+def simulation(days):
+    fish = Counter(numbers)
 
-for _ in range(80):
-    next_fish = Counter()
-    for timer, count in fish.items():
-        if timer == 0:
-            next_fish[8] += fish[timer]
-            next_fish[6] += fish[timer]
-        else:
-            next_fish[timer - 1] += fish[timer]
-    fish = next_fish
+    for _ in range(days):
+        next_fish = Counter()
+        for timer, count in fish.items():
+            if timer == 0:
+                next_fish[8] += fish[timer]
+                next_fish[6] += fish[timer]
+            else:
+                next_fish[timer - 1] += fish[timer]
+        fish = next_fish
 
-print(len(list(fish.elements())))
+    print(sum(fish.values()))
+
+# Part 1
+simulation(80)
+
+# Part 2
+simulation(256)

9452c8fb2c89007b6556871004057271175e8dc6
Thiago Perrotta <tbperrotta@gmail.com>
2021-12-10 22:43:07 -0500

    add day 7 part 2: python

--- day7/main.py
+++ day7/main.py
@@ -5,7 +5,22 @@ import sys
 with open(sys.argv[1]) as input:
     numbers = list(map(int, input.read().split(',')))
 
-median = int(statistics.median(numbers))
-fuel = sum(abs(median - number) for number in numbers)
+def part1():
+    median = int(statistics.median(numbers))
+    fuel = int(sum(abs(median - number) for number in numbers))
 
-print(fuel)
+    print(fuel)
+
+def cost(n):
+    return (n * (n + 1)) / 2
+
+def part2():
+    fuel = float('inf')
+
+    for guess in range(min(numbers), max(numbers) + 1):
+        fuel = min(int(sum(cost(abs(guess - number)) for number in numbers)), fuel)
+
+    print(fuel)
+
+part1()
+part2()

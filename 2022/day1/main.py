f9e9963cb39c3a49d461fb826b74ac156cb8b6de
Thiago Perrotta <tperrotta@chromium.org>
2022-12-01 14:36:22 +0100

    day 1 part 1

---
+++ 2022/day1/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import itertools
+import sys
+
+
+def main():
+    with open(sys.argv[1]) as input:
+        lines = input.read().splitlines()
+
+    # ['1', '2', '', '3'] -> [1, 2, '', 3]
+    lines = [int(line) if line != "" else line for line in lines]
+
+    # [1, 2, '', 3] -> [[1, 2], [3]]
+    groups = [list(group) for key, group in itertools.groupby(lines, lambda a: a == "") if not key]
+
+    print(max([sum(group) for group in groups]))
+
+
+if __name__ == '__main__':
+    main()

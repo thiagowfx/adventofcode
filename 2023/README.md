9dc0da629f98235d3cb7e7dfaa176087aa58bf5d
Thiago Perrotta <tbperrotta@gmail.com>
2023-12-04 20:40:36 +0100

    day 1 part 2

--- 2023/README.md
+++ 2023/README.md
@@ -1,6 +1,6 @@
 # Advent of Code 2023 - My Solutions
 
-![Completion Status](https://img.shields.io/badge/stars%20⭐-0/50-yellow)
+![Completion Status](https://img.shields.io/badge/stars%20⭐-2/50-yellow)
 
 Something is wrong with global snow production, and you've been selected to take a look. The Elves have even given you a map; on it, they've used stars to mark the top fifty locations that are likely to be having problems.
 
--- 2023/day1/main.py
+++ 2023/day1/main.py
@@ -12,12 +12,69 @@ def get_calibration_value(line: str) -> int:
     return int(digits[0] + digits[-1])
 
 
+SPELLINGS = {
+    # 'zero': '0',  # not in the problem statement
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+}
+
+
+# This attempt is incorrect because of the following test case:
+#   eightwothree -> eigh23 -> 23
+# Whereas it should have been 83.
+def get_calibration_value_with_spellings_attempt_1(line: str) -> int:
+    for spelling, digit in SPELLINGS.items():
+        line = line.replace(spelling, digit)
+
+    return get_calibration_value(line)
+
+
+def get_calibration_value_with_spellings(line: str) -> int:
+    targets = set(list(SPELLINGS.keys()) + list(SPELLINGS.values()))
+
+    # from the left
+    indices = {}
+    for target in targets:
+        index = line.find(target)
+        if index != -1:
+            indices[target] = index
+
+    [target_min, _] = min(list(indices.items()), key=lambda x: x[1])
+
+    # from the right
+    indices = {}
+    for target in targets:
+        index = line.rfind(target)
+        if index != -1:
+            indices[target] = index
+
+    [target_max, _] = max(list(indices.items()), key=lambda x: x[1])
+
+    if target_min in SPELLINGS:
+        target_min = SPELLINGS[target_min]
+
+    if target_max in SPELLINGS:
+        target_max = SPELLINGS[target_max]
+
+    return int(target_min + target_max)
+
+
 def main():
     with open(sys.argv[1]) as input:
         lines = input.read().splitlines()
 
     # Part I
     print(sum([get_calibration_value(line) for line in lines]))
+
+    # Part II
+    print(sum([get_calibration_value_with_spellings(line) for line in lines]))
 
 
 if __name__ == '__main__':

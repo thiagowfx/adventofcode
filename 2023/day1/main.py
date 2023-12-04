088291fdb3e7590f8f18b263a4b0d351af660205
Thiago Perrotta <tbperrotta@gmail.com>
2023-12-04 20:12:26 +0100

    day 1 part 1

--- 2023/day1/main.py
+++ 2023/day1/main.py
@@ -2,11 +2,22 @@
 import sys
 
 
+def get_calibration_value(line: str) -> int:
+    """
+    Extract the first and the last digit from the string, and return a number
+    composed by them, in that order.
+    '1abc2' -> 12
+    """
+    digits = [c for c in line if c.isdigit()]
+    return int(digits[0] + digits[-1])
+
+
 def main():
     with open(sys.argv[1]) as input:
         lines = input.read().splitlines()
 
-    print(lines)
+    # Part I
+    print(sum([get_calibration_value(line) for line in lines]))
 
 
 if __name__ == '__main__':

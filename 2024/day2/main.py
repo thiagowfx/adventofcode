05c09dc42a884eb56eef121d9be0cf067553ca60
Thiago Perrotta <tbperrotta@gmail.com>
2024-12-03 01:14:42 +0100

    day 2 part 2

--- 2024/day2/main.py
+++ 2024/day2/main.py
@@ -9,13 +9,40 @@ def main():
         lines = input.read().splitlines()
 
     safe = 0
+    safe_damp = 0
+
+    def is_safe(diffs):
+        return all(1 <= n <= 3 for n in diffs) or all(-3 <= n <= -1 for n in diffs)
 
     for line in lines:
-        diffs = [(a - b) for (a,b) in pairwise(map(int, line.split(' ')))]
-        safe += all(1 <= n <= 3 for n in diffs) or all(-3 <= n <= -1 for n in diffs)
+        l = list(map(int, line.split(' ')))
+        diffs = [(b - a) for (a,b) in pairwise(l)]
+
+        is_this_safe = is_safe(diffs)
+        if is_this_safe:
+            safe += 1
+            safe_damp += 1
+            continue
+
+        for i in range(len(l)):
+            if i == 0:
+                if is_safe(diffs[1:]):
+                    safe_damp += 1
+                    break
+            elif i == len(l) - 1:
+                if is_safe(diffs[:-1]):
+                    safe_damp += 1
+                    break
+            else:
+                if is_safe(diffs[:i-1] + [l[i+1] - l[i-1]] + diffs[i+1:]):
+                    safe_damp += 1
+                    break
 
     # part one
     print(safe)
+
+    # part two
+    print(safe_damp)
 
 
 if __name__ == '__main__':

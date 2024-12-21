01fb0ce27d18b97ba8fc3a0e6315db79e1342129
Thiago Perrotta <tbperrotta@gmail.com>
2024-12-20 22:47:26 -0300

    day 8 part 2

--- 2024/day8/main.py
+++ 2024/day8/main.py
@@ -4,8 +4,11 @@ from itertools import combinations
 
 import sys
 
+def within_bounds(x, y, height, width):
+    return 0 <= x < height and 0 <= y < width
 
-def compute_antinodes(coord1, coord2, height, width):
+
+def compute_antinodes(coord1, coord2, height, width, unbounded=False):
     x1, y1 = coord1
     x2, y2 = coord2
 
@@ -14,16 +17,20 @@ def compute_antinodes(coord1, coord2, height, width):
 
     dy = y2 - y1
 
-    x3, y3 = x1 - dx, y1 - dy
-    x4, y4 = x2 + dx, y2 + dy
+    if unbounded:
+        antinodes = ((x1, y1), (x2, y2))
+    else:
+        antinodes = ()
 
-    antinodes = ()
-
-    if 0 <= x3 < height and 0 <= y3 < width:
-        antinodes += ((x3, y3),)
-
-    if 0 <= x4 < height and 0 <= y4 < width:
-        antinodes += ((x4, y4),)
+    for (x0, y0, direction) in ((x1, y1, -1), (x2, y2, +1)):
+        steps = 1
+        while True:
+            x, y = x0 + direction * steps * dx, y0 + direction * steps * dy
+            if within_bounds(x, y, height, width):
+                antinodes += ((x, y),)
+                steps += 1
+            else:
+                break
 
     return antinodes
 
@@ -32,14 +39,14 @@ def main():
     with open(sys.argv[1]) as input:
         lines = input.read().splitlines()
 
-    antinodes = set()
-
     # {'0': ((1,8), ...)}
     freq_map = defaultdict(tuple)
     for x, line in enumerate(lines):
         for y, field in enumerate(line):
             if field.isalnum():
                 freq_map[field] += ((x, y),)
+
+    antinodes = set()
 
     for all_coords in freq_map.values():
         for coord1, coord2 in combinations(all_coords, 2):
@@ -48,6 +55,14 @@ def main():
     # part one
     print(len(antinodes))
 
+    antinodes = set()
+
+    for all_coords in freq_map.values():
+        for coord1, coord2 in combinations(all_coords, 2):
+            antinodes.update(compute_antinodes(coord1, coord2, len(lines), len(lines[0]), unbounded=True))
+
+    # part two
+    print(len(antinodes))
 
 if __name__ == '__main__':
     main()

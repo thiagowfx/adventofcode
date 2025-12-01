e5c41cc4645bf23c062404fbf803bb224bfb76ca
Thiago Perrotta <thiago@perrotta.dev>
2025-12-01 02:32:03 -0300

    2025 day 1

--- 2025/day1/main.py
+++ 2025/day1/main.py
@@ -5,7 +5,23 @@ def main():
     with open(sys.argv[1]) as input:
         lines = input.read().splitlines()
 
-    ...
+    ans = 0
+    dial = 50
+
+    for line in lines:
+        direction = line[0]
+        assert direction in 'LR'
+
+        delta = int(line[1:])
+        assert delta >= 0
+
+        dial = (dial + (-1 if direction == 'L' else 1) * delta + 100) % 100
+        # print(direction, delta, dial)
+
+        if dial == 0:
+            ans += 1
+
+    print(ans)
 
 
 if __name__ == '__main__':
